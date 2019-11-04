5a1587ac17b76ecb31d551be5fe16771510b162f
Cody M Leff <mail@codymleff.com>
2019-11-03 19:11:12 -0800

    Styled tablet/desktop figure modal (no margin links).

--- static-src/js/Modal/FigureModal.js
+++ static-src/js/Modal/FigureModal.js
@@ -1,20 +1,24 @@
 import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
+import classNamesBind from 'classnames/bind';
 
 import style from 'styles/Modal.module.css';
 import { Content } from '../markdown';
 import SectionContext from '../SectionContext';
 
 
+const cx = classNamesBind.bind(style);
+
 export function FigureModalBackground() {
   const { figureContentIdentifier } = useSelector((state) => state.modalContent);
   const { figureContentByIdentifier } = useContext(SectionContext);
   const figureContentNodes = figureContentByIdentifier[figureContentIdentifier].contentNodes;
+  const isImage = figureContentNodes[0].component === 'img';
 
   return (
-    <figure className={style.figureContent}>
+    <div className={cx(style.figureContent, { isImage })}>
       <Content nodes={figureContentNodes} />
-    </figure>
+    </div>
   );
 }
 
